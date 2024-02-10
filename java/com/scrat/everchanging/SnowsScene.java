029143ef1475c358a4f525df399b79449f061424
Yaroslav Mytkalyk <docd1990@gmail.com>
2024-02-10 21:54:36 +0100

    Increase FPS for SnowsScene to 40

--- java/com/scrat/everchanging/SnowsScene.java
+++ java/com/scrat/everchanging/SnowsScene.java
@@ -14,6 +14,11 @@ final class SnowsScene extends Scene {
     }
 
     @Override
+    public int getFps() {
+        return 40;
+    }
+
+    @Override
     public boolean hasObjectsInUse() {
         return show.objects.objectsInUseCount() != 0;
     }
